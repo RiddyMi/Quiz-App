0517aa725808c2daefc625ffc8fd8c9b856ed838
RiddyMi <rakande02@gmail.com>
2023-09-07 07:38:07 +0100

    Adding Quiz App

---
+++ questions.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        question: "What do you think is my favourite colour?",
+        answers: [
+            {option: "Red", correct: false},
+            {option: "Green", correct: true},
+            {option: "Blue", correct: false},
+        ],
+    },
+    
+    {
+        question: "What do you think is my favourite surah?",
+        answers: [
+            {option: "Baqarah", correct: false},
+            {option: "Insaan", correct: true},
+            {option: "Dukhaan", correct: false},
+        ],
+    },
+    
+    {
+        question: "What course am I studying?",
+        answers: [
+            {option: "Biomedical Eng", correct: false},
+            {option: "Civil $ Environmental Eng", correct: true},
+            {option: "Chemical Eng", correct: false},
+        ],
+    },
+    
+    {
+        question: "What is my surname?",
+        answers: [
+            {option: "Akande", correct: true},
+            {option: "Akinade", correct: false},
+            {option: "Akinde", correct: false},
+        ],
+    },
+    
+    {
+        question: "What is my best food",
+        answers: [
+            {option: "Rice and Beans", correct: true},
+            {option: "Pap and Akara", correct: false},
+            {option: "Bread and Beans", correct: false},
+        ],
+    },
+    ]
